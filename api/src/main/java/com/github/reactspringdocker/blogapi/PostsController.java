a2ad21e96a2c7a969d8252607ed5f55d36f764dd
Vitor Cavalcanti de Albuquerque <valbuq@thoughtworks.com>
2018-10-09 11:19:35 -0300

    Enables cross-origin requests

--- api/src/main/java/com/github/reactspringdocker/blogapi/PostsController.java
+++ api/src/main/java/com/github/reactspringdocker/blogapi/PostsController.java
@@ -3,6 +3,7 @@ package com.github.reactspringdocker.blogapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/posts")
 public class PostsController {
 
